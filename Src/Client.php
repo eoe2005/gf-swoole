113a92252e8425e8db28df404e9bc2c71a08a3bf
耿鸿飞 <genghongfei@soyoung.com>
2020-08-25 16:02:19 +0800

    fx

--- Src/Client.php
+++ Src/Client.php
@@ -4,12 +4,56 @@
 namespace Ghf;
 
 
+use function GuzzleHttp\json_encode;
+
 class Client
 {
-    static function Tcp($app,$cmd,$args){
+    public static function Rpc($app,$cmd,$args){
+        $host = self::getHost($app);
+        $ret = '';
+        if(strpos($host,'http') === 0){
+            return self::http($host,$cmd,$args);
+        }else{
+            return self::tcp($host,$cmd,$args);
+        }
+    }
+    private static function tcp($host,$cmd,$args){
+        try{
+            $fp = stream_socket_client($host,$error,$errmsg,5);
+            $sendData = [
+                'cmd' => $cmd,
+                'args' => $args
+            ];
+            fwrite($fp,json_encode($sendData)."\n");
+            $ret = fgetss($fp);
+            return json_decode($ret,true);
+        }catch (\Exception $e){
+            return ['code' => 1005,'msg' => $e->getTraceAsString()];
+        }
 
     }
-    static function Http($app,$cmd,$args){
+    private static function http($host,$cmd,$args){
+        $ch = curl_init($host.$cmd);
+        curl_setopt_array($ch,[
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $args,
+            CURLOPT_TIMEOUT_MS => 5000
+        ]);
+        try{
+            $ret = curl_exec($ch);
+            if(curl_errno($ch)){
+                return ['code' => 1005,'msg' => json_encode(curl_getinfo($ch))];
+            }
+            return json_decode($ret,true);
+        }catch (\Exception $e){
+            return ['code' => 1005,'msg' => $e->getTraceAsString()];
+        }
 
+
+    }
+
+    private static function getHost($app){
+        return Conf::Get('server.'.$app.'.host');
     }
 }
